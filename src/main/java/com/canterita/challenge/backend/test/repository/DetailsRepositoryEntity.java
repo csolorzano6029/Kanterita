ca1875b5a1ce9a994331b98a4eb8086d6974b5c5
csolorzano6029 <csolorzano6029@gmail.com>
2021-07-18 18:31:09 -0500

    Creacion de la interface DetailsRepositoryEntity con sus respectivos metodos dentro del directorio repository

--- src/main/java/com/canterita/challenge/backend/test/repository/DetailsRepositoryEntity.java
+++ src/main/java/com/canterita/challenge/backend/test/repository/DetailsRepositoryEntity.java
@@ -1,15 +1,14 @@
 package com.canterita.challenge.backend.test.repository;
 
-import java.util.ArrayList;
-import java.util.Optional;
-
+import java.util.*;
 import com.canterita.challenge.backend.test.model.DetailsEntity;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DetailsRepositoryEntity extends JpaRepository<DetailsEntity, Long> {
+    
     public abstract ArrayList<DetailsEntity> findByidOrder(Long id);
     public abstract void save(Optional<DetailsEntity> optional);
+    
 }
--- src/main/java/com/canterita/challenge/backend/test/repository/OrderRepository.java
+++ src/main/java/com/canterita/challenge/backend/test/repository/OrderRepository.java
@@ -2,15 +2,14 @@ package com.canterita.challenge.backend.test.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
-
 import com.canterita.challenge.backend.test.model.OrderEntity;
 
 @Repository
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
+    
     public abstract Optional<OrderEntity> findById(Long id);
-    default Optional<OrderEntity> getById(Long id){
-    	return  findById(id);
+    default Optional<OrderEntity> getById(Long id) {
+        return  findById(id);
     }
 }
